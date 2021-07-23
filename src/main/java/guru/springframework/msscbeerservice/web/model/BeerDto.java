221acb495bed85baed23e529f1c5f400cdad656f
Elhadji Barry <elhadji.barry@gmail.com>
2021-07-22 21:24:45 -0400

    Section 3 - Beer Service Model + Controller

---
+++ src/main/java/guru/springframework/msscbeerservice/web/model/BeerDto.java
@@ -0,0 +1,49 @@
+package guru.springframework.msscbeerservice.web.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class BeerDto implements Serializable {
+
+    //@Null
+    private UUID id;
+
+    //@Null
+    private Integer version;
+
+    //@Null
+    //@JsonFormat(pattern="yyyy-MM-dd'T'HH:mm:ssZ", shape=JsonFormat.Shape.STRING)
+    private OffsetDateTime createdDate;
+
+    //@Null
+    //@JsonFormat(pattern="yyyy-MM-dd'T'HH:mm:ssZ", shape=JsonFormat.Shape.STRING)
+    private OffsetDateTime lastModifiedDate;
+
+    //@NotBlank
+    private String beerName;
+
+    //@NotNull
+    private BeerStyle beerStyle;
+
+    //@NotNull
+    private Long upc;
+
+    //@JsonFormat(shape = JsonFormat.Shape.STRING)
+    //@Positive
+    //@NotNull
+    private BigDecimal price;
+
+    private Integer quantityOnHand;
+
+}
